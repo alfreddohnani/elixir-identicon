878b393c5894f88c17282d25e260aab11693c20d
Alfred Doh-Nani <alfreddohnani@hotmail.com>
2024-02-25 08:43:28 +0000

    using maps, IO.inspect and Enum.with_lists

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,10 +3,28 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> IO.inspect(charlists: :as_lists)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    (row ++ [second, first])
+    |> IO.inspect(charlists: :as_lists)
   end
 
   def hash_input(input) do
